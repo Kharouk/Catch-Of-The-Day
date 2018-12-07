b3a17b03b87e77077a80488982b3680010afbc40
Alex Kharouk <akharoukpp@gmail.com>
2018-12-07 12:07:14 +0000

    FEATURE Users can add an infinite amount of fishes to order.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ An application built using React and Firebase.
 ## What I've Learned:
 ### React:
 React Router, differences between props and state, passing down methods for child components to use. How to use refs and this-binding. 
+### Firebase:
+
 ### JavaScript:
-Object destructuring.
+Object destructuring, object spread.
 ### Deployment:
 Netlify, Now, and Apache Server.
--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,15 @@ export default class App extends React.Component {
     this.setState({ fishes });
   };
 
+  addToOrder = key => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the order
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state object
+    this.setState({ order });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
@@ -31,7 +40,12 @@ export default class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
