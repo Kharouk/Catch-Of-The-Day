38e862604f79c6a0552bc226d97c8540b73e4a1a
Alex Kharouk <akharoukpp@gmail.com>
2018-12-07 10:13:23 +0000

    ADD Fish state and now users can create fish and update menu

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 export default class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // the ... is an object spread that makes a copy
+    const fishes = { ...this.state.fishes };
+    // add our new fishes to the fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +25,7 @@ export default class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
